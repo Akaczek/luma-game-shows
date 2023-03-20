eeb35739a8a64d796459179564497ce2819e999a
martyna <jmartyna1810@gmail.com>
2023-03-20 11:39:02 +0100

    add answer Page

--- src/pages/participant/answerPage.js
+++ src/pages/participant/answerPage.js
@@ -1,37 +1,20 @@
-import styles from "../../styles/participant/questionPage.module.css";
-import QuizAnswers from "@/components/participant/quizAnswers";
-import { createAnswerLetters, calculateAnswerHeight } from "@/utils/answers";
-import Image from "next/image";
-import Link from "next/link";
-import exitImageFile from "../../../public/exit.svg";
-import OpenAnswer from "@/components/participant/openAnswer";
+import styles from "../../styles/participant/answerPage.module.css";
 
 const QuestionPage = () => {
-    const answersQuantity = 4;
-    const answers = createAnswerLetters(answersQuantity);
-    const isAnswerBig = calculateAnswerHeight(answersQuantity);
-    const correctAnswer = "B";
+    const isCorrect = true;
+
     return (
-        <>
-            <Link href="/" className={styles.exitImageLink}>
-                <Image
-                    src={exitImageFile}
-                    alt="exit"
-                    className={styles.exitImage}
-                />
-            </Link>
-            <div className={styles.questionPageContainer}>
-                <div className={styles.question}>
-                    <div className={styles.questionText}>
-                        Jaki jest najwyższy szczyt na świecie?
-                    </div>
-                </div>
-                <QuizAnswers
-                    answerLetters={answers}
-                    isAnswerBig={isAnswerBig}
-                />
+        <div className={isCorrect ? styles.correctAnswer : styles.wrongAnswer}>
+            <div className={styles.answerLabelRow}>
+                {isCorrect ? "Poprawna odpowiedź" : "Zła odpowiedź"}
             </div>
-        </>
+            <div className={styles.answerLabelRow}>
+                {isCorrect ? ":)" : ":("}
+            </div>
+            <div className={styles.waitingLabelRow}>
+                Oczekiwanie na następne pytanie...
+            </div>
+        </div>
     );
 };
 
