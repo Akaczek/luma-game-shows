60872405f70291bdacd8443c745929d93f215ff6
Akaczek <luk12349876@o2.pl>
2023-06-14 15:10:57 +0200

    minor fixes

--- src/components/presenter/resultsOfOpenQuestions.js
+++ src/components/presenter/resultsOfOpenQuestions.js
@@ -37,7 +37,7 @@ const ResultsOfOpenQuestions = ({handleSendAnswers, players}) => {
           >
             <div className={styles.userInfo}>
               <span className={styles.userNick}>{result.userNick}</span>
-              <label class="checkboxContainer">
+              <label className="checkboxContainer">
                 <input
                   type="checkbox"
                   className={styles.correctCheckbox}
